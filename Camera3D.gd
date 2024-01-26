bf728bf4e2131a9de038bdd4786c8d0572c10f8a
LuisSilva-dev <lcd.silva@campus.fct.unl.pt>
2024-01-26 23:54:36 +0000

    Player can move and look around and jump.
    Walk with (WASD), jump with SPACE and look with the mouse.

---
+++ Camera3D.gd
@@ -0,0 +1,21 @@
+extends Camera3D
+
+var mouse_sens = 0.3
+var camera_anglev=0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func _input(event):  		
+	if event is InputEventMouseMotion:
+		$Camera.rotate_y(deg_to_rad(-event.relative.x*mouse_sens))
+		var changev=-event.relative.y*mouse_sens
+		if camera_anglev+changev>-50 and camera_anglev+changev<50:
+			camera_anglev+=changev
+			$Camera.rotate_x(deg_to_rad(changev))
