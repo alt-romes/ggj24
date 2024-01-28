96965f4214db45420c9374b716f7be36a31a8b70
Rodrigo Mesquita <rodrigo.m.mesquita@gmail.com>
2024-01-28 16:38:28 +0000

    SFDSFMSDKLF

---
+++ Main.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+#@onreadu 
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+	#total = $FirstRow.get_child_count()
+	#total += $FirstRow2.get_child_count()
+	#total += $FirstRow3.get_child_count()
+	#total += $FirstRow4.get_child_count()
+	#total += $FirstRow5.get_child_count()
+	#total += $FirstRow6.get_child_count()
+	#total += $FirstRow7.get_child_count()
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	#for i in total:
+		#if get_node("FirstRow"+str(i)).state == :
